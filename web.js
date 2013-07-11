0807a28c3d5e5b348b6cfa25032905f614d57117
Ubuntu <nebeleh@gmail.com>
2013-07-11 22:20:59 +0000

    adding a web.js to act as a node server for loading hello.html file

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var buffer = fs.readFileSync('hello.html');
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
