2a124087c8b96f3e3efc1a764f2f67d7cef586f9
Ubuntu <nebeleh@gmail.com>
2013-07-16 22:29:20 +0000

    test

--- web.js
+++ web.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
+app.configure(function() {
+  app.use('/js', express.static(__dirname + '/js'));
+})
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
