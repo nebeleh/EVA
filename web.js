e5709cd310305938488d6e2533aa3cf5d816d53c
Ubuntu <nebeleh@gmail.com>
2013-07-16 21:32:07 +0000

    test

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('webgl/index.html');
+  var buffer = fs.readFileSync('webgl/test.html');
   response.send(buffer.toString());
 });
 
