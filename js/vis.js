8907653e07d66036e4670d9981ae4752f05fe43f
Ubuntu <nebeleh@gmail.com>
2013-07-17 20:01:36 +0000

    lights

--- js/vis.js
+++ js/vis.js
@@ -12,7 +12,7 @@ function init($container) {
 
   scene.add(camera);
   
-  var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+  /*var light = new THREE.AmbientLight( 0x404040 ); // soft white light
   scene.add(light);
 
   var pl1 = new THREE.PointLight( 0xFFFFFF );
@@ -21,11 +21,44 @@ function init($container) {
   pl1.position.z = 130;
   scene.add(pl1);
 
-  /*var pl2 = new THREE.PointLight( 0xFFFFFF );
+  var pl2 = new THREE.PointLight( 0xFFFFFF );
   pl2.position.x = 10;
   pl2.position.y = -50;
   pl2.position.z = 130;
   scene.add(pl2);*/
+  
+  // LIGHTS
+
+  hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+  hemiLight.color.setHSL( 0.6, 1, 0.6 );
+  hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+  hemiLight.position.set( 0, 500, 0 );
+  scene.add( hemiLight );
+
+  //
+
+  dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  dirLight.color.setHSL( 0.1, 1, 0.95 );
+  dirLight.position.set( -1, 1.75, 1 );
+  dirLight.position.multiplyScalar( 50 );
+  scene.add( dirLight );
+
+  dirLight.castShadow = true;
+
+  dirLight.shadowMapWidth = 2048;
+  dirLight.shadowMapHeight = 2048;
+
+  var d = 50;
+
+  dirLight.shadowCameraLeft = -d;
+  dirLight.shadowCameraRight = d;
+  dirLight.shadowCameraTop = d;
+  dirLight.shadowCameraBottom = -d;
+
+  dirLight.shadowCameraFar = 3500;
+  dirLight.shadowBias = -0.0001;
+  dirLight.shadowDarkness = 0.35;
+  //dirLight.shadowCameraVisible = true;
 }
 
 function addShape() {
@@ -34,7 +67,7 @@ function addShape() {
   scene.add(mesh);
 }
 
-var x = 1.3, y = 1.2, z = 1.1;
+var x = .3, y = .2, z = .1;
 
 function animate() {
   requestAnimationFrame(animate);
