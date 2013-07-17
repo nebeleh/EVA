dbd62358e97b07b8548e74e915a6c44c01b98bcb
Ubuntu <nebeleh@gmail.com>
2013-07-17 20:05:40 +0000

    cleaned

--- js/vis.js
+++ js/vis.js
@@ -6,55 +6,33 @@ function init($container) {
   renderer = new THREE.WebGLRenderer();
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
   scene = new THREE.Scene();
-  camera.position.z = 300;
+  camera.position.z = 600;
   renderer.setSize(WIDTH, HEIGHT);
   $container.append(renderer.domElement);
 
   scene.add(camera);
-  
-  /*var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-  scene.add(light);
 
-  var pl1 = new THREE.PointLight( 0xFFFFFF );
-  pl1.position.x = 10;
-  pl1.position.y = 50;
-  pl1.position.z = 130;
-  scene.add(pl1);
-
-  var pl2 = new THREE.PointLight( 0xFFFFFF );
-  pl2.position.x = 10;
-  pl2.position.y = -50;
-  pl2.position.z = 130;
-  scene.add(pl2);*/
-  
   // LIGHTS
-
   hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
   hemiLight.color.setHSL( 0.6, 1, 0.6 );
   hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
   hemiLight.position.set( 0, 500, 0 );
   scene.add( hemiLight );
 
-  //
-
   dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
   dirLight.color.setHSL( 0.1, 1, 0.95 );
   dirLight.position.set( -1, 1.75, 1 );
   dirLight.position.multiplyScalar( 50 );
   scene.add( dirLight );
-
   dirLight.castShadow = true;
-
   dirLight.shadowMapWidth = 2048;
   dirLight.shadowMapHeight = 2048;
 
   var d = 50;
-
   dirLight.shadowCameraLeft = -d;
   dirLight.shadowCameraRight = d;
   dirLight.shadowCameraTop = d;
   dirLight.shadowCameraBottom = -d;
-
   dirLight.shadowCameraFar = 3500;
   dirLight.shadowBias = -0.0001;
   dirLight.shadowDarkness = 0.35;
@@ -67,14 +45,14 @@ function addShape() {
   scene.add(mesh);
 }
 
-var x = .3, y = .2, z = .1;
+var x = .3, y = .2, z = .1, w = 100;
 
 function animate() {
   requestAnimationFrame(animate);
   
-  if (mesh.position.x > 50 || mesh.position.x < -50) { x *= -1; }
-  if (mesh.position.y > 50 || mesh.position.y < -50) { y *= -1; }
-  if (mesh.position.z > 50 || mesh.position.z < -50) { z *= -1; }
+  if (mesh.position.x > w || mesh.position.x < -w) { x *= -1; }
+  if (mesh.position.y > w || mesh.position.y < -w) { y *= -1; }
+  if (mesh.position.z > w || mesh.position.z < -w) { z *= -1; }
   // playing with the ball
   mesh.position.x += x;
   mesh.position.y += y;
