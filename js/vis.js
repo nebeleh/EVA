ec91d1328b52b371c5f0c5386c9c5d1ed22ae117
Ubuntu <nebeleh@gmail.com>
2013-08-01 00:18:51 +0000

    working on coil visualization

--- js/vis.js
+++ js/vis.js
@@ -1,6 +1,6 @@
 // used code from http://stemkoski.github.io/Three.js/Graphulus-Surface.html
 
-var renderer, camera, scence, controls, mesh, stats, material;
+var renderer, camera, scence, controls, stats;
 
 function init($container) {
   // scene
@@ -18,6 +18,7 @@ function init($container) {
   var WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
   var VIEW_ANGLE = 45, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 10000;
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  //camera = new THREE.OrthographicCamera(WIDTH/-2, WIDTH/2, HEIGHT/2, HEIGHT/-2, NEAR, FAR);
   camera.position.set(-10,-10,10);
   camera.lookAt(scene.position);  
   camera.up = new THREE.Vector3( 0, 0, 1 );
@@ -78,14 +79,56 @@ function init($container) {
   var floorMaterial = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true, side: THREE.DoubleSide});
   var floorGeom = new THREE.PlaneGeometry(20,20,20,20);
   var floor = new THREE.Mesh(floorGeom, floorMaterial);
-  scene.add(floor);
+  //scene.add(floor);
   
   // start drawing
-  drawArray();
+  draw();
   animate();
 }
 
-function calcWindowResize(renderer, camera) {
+function draw()
+{
+  // making a coil
+  var points = [], i = 0, r = 4, f = 1, minZ = -10, maxZ = 10;
+  for (var z = minZ; z <= maxZ; z += 0.1/f)
+  {
+    points[i++] = new THREE.Vector3(r*Math.sin(z*f*Math.PI), r-r*Math.cos(z*f*Math.PI), z);
+  }
+
+  // creating the spline
+  var geometry = new THREE.Geometry();
+  var segments = 10;
+  var spline = new THREE.Spline(points);
+
+  var index, position, colors = [];
+  for (var i=0; i<points.length * segments; i++)
+  {
+    index = i / (points.length * segments);
+    position = spline.getPoint(index);
+    geometry.vertices[i] = new THREE.Vector3(position.x, position.y, position.z);
+    colors[i] = new THREE.Color(0xffffff);
+    (position.z > 0) ? colors[i].setHSL(0.3, 1.0, 0.75*position.z/maxZ) : colors[i].setHSL(0.6, 1.0, -0.75*position.z/maxZ);
+  }
+
+  geometry.colors = colors;
+  var material = new THREE.LineBasicMaterial({color: 0xffffff, opacity: 1,  linewidth: 3, vertexColors: THREE.VertexColors});
+
+  var line = new THREE.Line(geometry, material);
+  scene.add(line);
+
+  // adding datapoints
+  var dataMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.6});
+  for (var i = 0; i < points.length; i++)
+  {
+    if (Math.random() < 0.7) continue;
+    var dataMesh = new THREE.Mesh(new THREE.SphereGeometry(0.2,100,100), dataMaterial);
+    dataMesh.position.set(points[i].x, points[i].y, points[i].z);
+    scene.add(dataMesh);
+  }
+}
+
+function calcWindowResize(renderer, camera)
+{
   var callback = function(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -99,95 +142,15 @@ function calcWindowResize(renderer, camera) {
   };
 }
 
-function drawArray() {
-  var datasize = 500;
-  var buffer = new ArrayBuffer(8*datasize);
-  var data = new Float64Array(buffer);
-  
-  // creating a random walk dataset 
-  data[0] = 0;
-  for (var i=1; i<data.length; i++) {
-    data[i] = data[i-1]+(Math.random()-0.5)*0.5;
-  }
-  
-  var geom = new arrayGeometry(data);
-
-  if (mesh) {
-    scene.remove(mesh);
-  }
-  
-  // front side
-  material = new THREE.MeshLambertMaterial({color: 0xFF0000, side: THREE.FrontSide});
-  mesh = new THREE.Mesh(geom,material);
-  scene.add(mesh);
-  
-  // back side -- for increasing speed, use THREE.DoubleSide and disable this part
-  material = new THREE.MeshLambertMaterial({color: 0xD358F7, side: THREE.BackSide});
-  mesh = new THREE.Mesh(geom,material);
-  scene.add(mesh);
-}
-
-// creates a 3D geometry based on the input array
-function arrayGeometry(data) {
-
-	THREE.Geometry.call( this );
-
-	var verts = this.vertices;
-	var faces = this.faces;
-	var uvs = this.faceVertexUvs[ 0 ];
-  
-  var xMax = 10, xMin = 0, yMax = 10, yMin = 0, segments = 500, iMin = 0;
-  var iMax, x, y, z;
-  var P = 0, Q = 0, R = 0;
-  var a,b,c,d,i;
-  for (y=yMin;y<=yMax;y+=(yMax-yMin)/segments)
-  {
-    iMax = (y-yMin)/(yMax-yMin)*(data.length-iMin)+iMin;
-    Q = verts.length;
-    for (i=0;i<iMax;i++)
-    {
-      x = i*(xMax-xMin)/iMax+xMin;
-      verts.push(new THREE.Vector3(x,y,data[i]));
-    }
-    verts.push(new THREE.Vector3(xMax,y,data[i-1]));
-    R = verts.length-1;
-    // adding faces
-    if (y>yMin)
-    {
-      var p;
-      for (p=P; p<=Q-2; p++)
-      {
-        a = p;
-        b = p+1;
-        c = p+(Q-P)+1;
-        d = p+(Q-P);
-        faces.push(new THREE.Face4(a,b,c,d));
-      }
-      a = p;
-      for (var q=Q-P+p;q<=R-1;q++)
-      {
-        b = q+1;
-        c = q;
-        faces.push(new THREE.Face3(a,b,c));
-      }
-      P = Q;
-    }
-  }
-
-	this.computeCentroids();
-	this.computeFaceNormals();
-	this.computeVertexNormals();
-}
-
-arrayGeometry.prototype = Object.create(THREE.Geometry.prototype);
-
-function animate() {
+function animate()
+{
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   update();
 }
 
-function update() {
+function update()
+{
   controls.update();
   stats.update();
 }
